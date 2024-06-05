a8498316b55f26337771a926a8152e3043bc9f80
Richard Felintro da Silva <richardfelintro06@gmail.com>
2024-06-05 14:47:45 -0300

    Merge consertado.

--- bibliotech/App.js
+++ bibliotech/App.js
@@ -3,31 +3,28 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Navigation } from "./src/screens/Navigation/Navigation";
 import {
   useFonts,
-  MontserratAlternates_100Thin,
   MontserratAlternates_200ExtraLight,
-  MontserratAlternates_100Thin_Italic,
-  MontserratAlternates_200ExtraLight_Italic,
-  MontserratAlternates_300Light,
-  MontserratAlternates_300Light_Italic,
   MontserratAlternates_400Regular,
-  MontserratAlternates_400Regular_Italic,
   MontserratAlternates_500Medium,
-  MontserratAlternates_500Medium_Italic,
   MontserratAlternates_600SemiBold,
-  MontserratAlternates_600SemiBold_Italic,
   MontserratAlternates_700Bold,
-  MontserratAlternates_700Bold_Italic,
-  MontserratAlternates_800ExtraBold,
-  MontserratAlternates_800ExtraBold_Italic,
-  MontserratAlternates_900Black,
-  MontserratAlternates_900Black_Italic,
 } from "@expo-google-fonts/montserrat-alternates";
 import { Main } from "./src/screens/Main/Main";
+import { StatusBar } from "expo-status-bar";
+
+//* Richard telas
+import { LoginScreen } from "./src/screens/LoginScreen/LoginScreen";
+import { ForgotPasswordScreen } from "./src/screens/ForgotPasswordScreen/ForgotPasswordScreen";
+import { ChangePasswordScreen } from "./src/screens/ChangePasswordScreen/ChangePasswordScreen";
+import { CodeVerificationScreen } from "./src/screens/CodeVerificationScreen/CodeVerificationScreen";
+import { BookInfoScreen } from "./src/screens/BookInfoScreen/BookInfoScreen";
+
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   const [fontsLoaded, fontError] = useFonts({
+    MontserratAlternates_200ExtraLight,
     MontserratAlternates_400Regular,
     MontserratAlternates_500Medium,
     MontserratAlternates_600SemiBold,
@@ -38,6 +35,7 @@ export default function App() {
   }
   return (
     <NavigationContainer>
+      <StatusBar translucent backgroundColor="transparent" />
       <Stack.Navigator>
         <Stack.Screen
           name="Navegation"
@@ -51,6 +49,51 @@ export default function App() {
         <Stack.Screen
           name="Main"
           component={Main}
+          options={{
+            title: "",
+            headerTransparent: true,
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{
+            title: "",
+            headerTransparent: true,
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="ForgotPassword"
+          component={ForgotPasswordScreen}
+          options={{
+            title: "",
+            headerTransparent: true,
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="CodeVerification"
+          component={CodeVerificationScreen}
+          options={{
+            title: "",
+            headerTransparent: true,
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="ChangePassword"
+          component={ChangePasswordScreen}
+          options={{
+            title: "",
+            headerTransparent: true,
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="BookInfo"
+          component={BookInfoScreen}
           options={{
             title: "",
             headerTransparent: true,
