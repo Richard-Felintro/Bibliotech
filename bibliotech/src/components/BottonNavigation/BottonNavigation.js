7fd64efc186ee564ec7dbd54e63e9c22ef2a8efe
Murilo Alves Zapiello <54750759805@info.local>
2024-06-05 15:07:08 -0300

    subedo o código

---
+++ bibliotech/src/components/BottonNavigation/BottonNavigation.js
@@ -0,0 +1,47 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { ContentIcon } from "./Style";
+import { SimpleLineIcons } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+
+const BottomTab = createBottomTabNavigator();
+
+export const BottomNavigation = () => {
+  return (
+    <BottomTab.Navigator
+      initialRouteName="Main"
+      screenOptions={() => ({
+        tabBarStyle: {
+          backgroundColor: "#012141",
+          height: 64,
+          paddingTop: 10,
+        },
+        tabBarActiveBackgroundColor: "transparent",
+        tabBarShowLabel: false,
+        headerShown: false,
+
+        tapBarIcon: ({ focused }) => {
+          return (
+            <>
+              <ContentIcon
+                tabBarActiveBackgroundColor={
+                  focused ? "#ffffff" : "transparent"
+                }
+              >
+                <SimpleLineIcons name="home" size={29} color="#ffffff" />
+              </ContentIcon>
+              <ContentIcon
+                tabBarActiveBackgroundColor={
+                  focused ? "#ffffff" : "transparent"
+                }
+              >
+                <Ionicons name="person" size={24} color="#ffffff" />
+              </ContentIcon>
+            </>
+          );
+        },
+      })}
+    >
+      <BottomTab.Screen name="Main" />
+    </BottomTab.Navigator>
+  );
+};
--- bibliotech/src/components/BottonNavigation/Style.js
+++ bibliotech/src/components/BottonNavigation/Style.js
@@ -1,19 +1,13 @@
-import { LinearGradient } from "expo-linear-gradient";
 import styled from "styled-components";
 
-export const GradientHeader = styled(LinearGradient).attrs({
-  colors: ["#2C7DA0", "#01497C", "#012141"],
-  start: {
-    x: 0,
-    y: 0,
-  },
-  end: {
-    x: 1,
-    y: 1,
-  },
-
+export const ContentIcon = styled.View.attrs({
   focus: true,
 })`
-  border-radius: 18px;
   padding: 9px 12px;
+  background-color: ${(props) => `${props.tapBarActiveBackgroundColor}`};
+
+  gap: 5px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
 `;
